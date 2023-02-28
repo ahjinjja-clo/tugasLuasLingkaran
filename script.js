a3946a9b5609b0b17e9eee951be550e323d3bfe1
N <113294876+ahjinjja-clo@users.noreply.github.com>
2023-02-28 11:52:31 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let jari; 
+const pi = 22/7;
+let total;
+
+function jumlahLuasLingkaran(a, b) {
+    jari = a **2;
+    b = 22/7;
+    total = jari * pi;
+    return total;
+}
+
+alert(jumlahLuasLingkaran(7,4));
